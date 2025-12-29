eaa7f78bc78ac8ee54c58b9f9c5a662808a23ca3
ms8096461-blip <ms8096461@gmail.com>
2025-12-29 12:57:06 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"status": "Trading AI Backend Running"}
+    @app.get("/signal")
+def signal():
+    return {
+        "pair": "EUR/USD",
+        "signal": "BUY",
+        "expiry": "1 minute",
+        "confidence": "80%"
+    }
